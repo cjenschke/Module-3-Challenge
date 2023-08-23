d749692121f1e88c7ae7b98e97ad14826045b816
cjenschke <cimarron13@mac.com>
2023-08-23 17:24:20 -0500

    reorganized code

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-
-
 var numberOfCharacters = 8;
 var userChoice = [];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-
 var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", writePassword);
+function userPrompts() {
+    userChoice = [];
+    numberOfCharacters = (prompt("Choose between 8-128 characters for you password."));
+
+    if (isNaN(numberOfCharacters) || numberOfCharacters < 8 || numberOfCharacters > 128) {
+        alert("Wrong! Password must be between 8 - 128 characters.");
+        return false;
+    }
+    if (confirm("Do you want to use numbers?")) {
+        userChoice = userChoice.concat(numbers);
+    } else {
+        false;
+    }
+    if (confirm("Do you want to use lowercase characters?")) {
+        userChoice = userChoice.concat(lowerCaseCharacters);
+    } else {
+        false;
+    }
+    if (confirm("Do you want to use UPPERCASE characters?")) {
+        userChoice = userChoice.concat(upperCaseCharacters);
+    } else {
+        false;
+    }
+    if (confirm("Do you want to use special characters?")) {
+        userChoice = userChoice.concat(specialCharacters);
+    } else {
+        false;
+    } return true;
+}
 
 function writePassword() {
     var correctPrompts = userPrompts();
@@ -33,33 +58,5 @@ function generatePassword() {
     return password;
 }
 
-function userPrompts() {
-    userChoice = [];
-    numberOfCharacters = parseInt(prompt("Choose between 8-128 characters for you password."));
+generateBtn.addEventListener("click", writePassword);
 
-    if (isNaN(numberOfCharacters) || numberOfCharacters < 8 || numberOfCharacters > 128) {
-        alert("Wrong! Password must be between 8 - 128 characters.");
-        return false;
-    }
-    if (confirm("Do you want to use numbers?")) {
-        userChoice = userChoice.concat(numbers);
-    } else {
-        false;
-    }
-    if (confirm("Do you want to use lowercase characters?")) {
-        userChoice = userChoice.concat(lowerCaseCharacters);
-    } else {
-        false;
-    }
-    if (confirm("Do you want to use uppercase characters?")) {
-        userChoice = userChoice.concat(upperCaseCharacters);
-    } else {
-        false;
-    }
-    if (confirm("Do you want to use special characters?")) {
-        userChoice = userChoice.concat(specialCharacters);
-    } else {
-        false;
-    } return true;
-
-}
